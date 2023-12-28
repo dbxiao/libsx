8f437ec72ffa6f80a1032c6477c24703d0bf0314
dbxiao <dbxiao@foxmail.com>
2023-12-28 10:17:16 +0800

    fix: footer add ICP addr

--- .dumirc.ts
+++ .dumirc.ts
@@ -14,6 +14,6 @@ export default defineConfig({
     outputPath: `dist/res/libsx`,
     themeConfig: {
         name: `cola`,
-        footer: `<p style='font-size: 12px; color: #666;'>Copyright © 2023 @libsx. MIT Licensed. Created by dbxiao</p>`
+        footer: `<p style='font-size: 12px; color: #666;'>Copyright © 2023 libsx.com | MIT Licensed | <a href="https://www.beianx.cn/" target="_blank" >渝ICP备2023017828号-1</a> | Created by <a href="https://github.com/dbxiao"  target="_blank">dbxiao</a> </p>`
     },
 });
